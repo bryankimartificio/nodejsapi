558b00868889f1664297fd9034e61900f51f2db9
bryankim.artificio@essilor.com <bryankim.artificio@essilor.com>
2019-06-26 20:07:28 +0800

    adding README file & middleware link exception

--- middlewares/jwt.middlewares.js
+++ middlewares/jwt.middlewares.js
@@ -3,11 +3,13 @@ require('dotenv').config()
 
 module.exports = function (options) {
   return function (req, res, next) {
-    let token = req.headers['x-access-token']
-    if (!token) return res.status(401).send({ statuscode: 400, message: 'No token provided.' })
-    jwt.verify(token, process.env.SECRET, function (err, decoded) {
-      if (err) return res.status(500).send({ statuscode: 401, message: 'Failed to authenticate token.' })
-    })
+    if (!req.originalUrl.toString().includes('register')) {
+      let token = req.headers['x-access-token']
+      if (!token) return res.status(401).send({ statuscode: 400, message: 'No token provided.' })
+      jwt.verify(token, process.env.SECRET, function (err, decoded) {
+        if (err) return res.status(500).send({ statuscode: 401, message: 'Failed to authenticate token.' })
+      })
+    }
     next()
   }
 }
